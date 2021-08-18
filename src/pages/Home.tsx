5cd93676c36340ceeb53fa9f485a75b104f1ed51
leoaraujosa <leoo_araujoo@hotmail.com>
2021-08-18 11:12:57 -0300

    Added feature of like and deleting questions and closing rooms

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,6 +39,11 @@ export function Home() {
             return
         }
 
+        if (roomRef.val().endedAt) {
+          alert('Room already closed')
+          return
+        }
+
         history.push(`/rooms/${roomCode}`)
     }
 
@@ -72,4 +77,5 @@ export function Home() {
             </main>
         </div>
     )
+    
 }
